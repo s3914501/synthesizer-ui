63c75f1dea6fdb3873aa0c235a5723d06041867f
s3914501 <s3914501@student.rmit.edu.au>
2025-08-20 12:17:10 +1000

    Update keyboardController.js

--- keyboardController.js
+++ keyboardController.js
@@ -10,6 +10,9 @@ let allKeys = Array.from(document.getElementsByClassName("whiteKey")).concat(
 /* set default octace : we will update based on keys later on */
 let octave = 3;
 
+let keyPressed = false;
+
+window.addEventListener("mouseup", () => {});
 /* add an event listener to each key */
 allKeys.forEach((key) => {
   key.addEventListener("mousedown", (e) => {
@@ -17,6 +20,14 @@ allKeys.forEach((key) => {
     polySynth.triggerAttackRelease(note + octave, "8n");
   });
   key.addEventListener("mouseup", (e) => {
+    let note = e.target.dataset.note;
+    polySynth.triggerRelease(note + octave);
+  });
+  key.addEventListener("mouseenter", (e) => {
+    if (keyPressed === false) {
+      return;
+    }
+
     let note = e.target.dataset.note;
     polySynth.triggerRelease(note + octave);
   });
